f555a4b040d092630ab5c921307007b4199056b0
Traitanit Huangsri <fanmanutd@gmail.com>
2020-02-23 19:26:42 +0700

    Add description for each LIFF api (#11)
    * add comments and some example usage
    
    * remove jsdoc syntax
    
    * remove-example
    
    * remove returns

--- index.d.ts
+++ index.d.ts
@@ -6,22 +6,29 @@ declare type LIFFConfig = {
 }
 
 declare type LIFFErrorObject = {
-    code: 'INIT_FAILED' | 'INVALID_ARGUMENT' | 'UNAUTHORIZED' | 'FORBIDDEN' | 'INVALID_CONFIG' | 'INVALID_ID_TOKEN' | 'INVALID_ARGUMENT' | 'THINGS_NO_LINKED_DEVICES' | 'BLUETOOTH_SETTING_OFF' | 'THINGS_TERMS_NOT_AGREED' | 'BLUETOOTH_NO_LOCATION_PERMISSION' | 'BLUETOOTH_LOCATION_DISABLED' | 'BLUETOOTH_LE_API_UNAVAILABLE' | 'BLUETOOTH_CONNECT_FAILED' | 'BLUETOOTH_ALREADY_CONNECTED' | 'BLUETOOTH_CONNECTION_LOST' | 'BLUETOOTH_UNSUPPORTED_OPERATION' | 'BLUETOOTH_SERVICE_NOT_FOUND' | 'BLUETOOTH_CHARACTERISTIC_NOT_FOUND',
+    code: 'INIT_FAILED' | 'INVALID_ARGUMENT' | 'UNAUTHORIZED' | 'FORBIDDEN' | 
+        'INVALID_CONFIG' | 'INVALID_ID_TOKEN' | 'THINGS_NO_LINKED_DEVICES' | 
+        'BLUETOOTH_SETTING_OFF' | 'THINGS_TERMS_NOT_AGREED' | 'BLUETOOTH_NO_LOCATION_PERMISSION' | 
+        'BLUETOOTH_LOCATION_DISABLED' | 'BLUETOOTH_LE_API_UNAVAILABLE' | 'BLUETOOTH_CONNECT_FAILED' | 
+        'BLUETOOTH_ALREADY_CONNECTED' | 'BLUETOOTH_CONNECTION_LOST' | 'BLUETOOTH_UNSUPPORTED_OPERATION' | 
+        'BLUETOOTH_SERVICE_NOT_FOUND' | 'BLUETOOTH_CHARACTERISTIC_NOT_FOUND',
     message: string,
 }
 
-declare type LiffInitSuccessCallback = (
-    data: {
-        language: string,
-        context: {
-            type: string,
-            viewType: string,
-            userId: string,
-            utouId: string,
-            roomId: string,
-            groupId: string,
-        }
+declare type LiffInitSuccessData = {
+    language: string,
+    context: {
+        type: string,
+        viewType: string,
+        userId: string,
+        utouId: string,
+        roomId: string,
+        groupId: string,
     }
+}
+
+declare type LiffInitSuccessCallback = (
+    data: LiffInitSuccessData
 ) => void
 
 declare type LiffInitErrorCallback = (error: LIFFErrorObject) => void
@@ -99,50 +106,118 @@ declare interface Friendship {
 
 declare global {
     namespace liff {
+        /**
+         * Initialize a liff app. You can only call other LIFF SDK methods after calling liff.init(). 
+         * The LIFF SDK gets access tokens and ID tokens from the LINE platform when you initialize the LIFF app.
+         * @param config - LIFF app ID. Can be obtained when you add the LIFF app to your channel. 
+         * @param initSuccessCallback - Callback to return a data object upon successful initialization of the LIFF app.
+         * @param errorCallback - Callback to return an error object upon failure to initialize the LIFF app.
+         * 
+         */
         function init(
             config: LIFFConfig,
             initSuccessCallback?: LiffInitSuccessCallback,
             errorCallback?: LiffInitErrorCallback,
         ): Promise<void>;
         
+        /**
+         * Gets the environment in which the user is running the LIFF app.
+         */
         function getOS(): 'ios' | 'android' | 'web'
 
+        /**
+         * Gets the version of the LIFF SDK.
+         */
         function getVersion(): string
 
+        /**
+         * Gets the language settings of the environment in which the LIFF app is running.
+         */
         function getLanguage(): string
 
+        /**
+         * Determines whether the LIFF app is running in LINE's in-app browser.
+         */
         function isInClient(): boolean
 
+        /**
+         * Checks whether the user is logged in.
+         */
         function isLoggedIn(): boolean
 
+        /**
+         * Performs the LINE Login process (web login) for the Web app.
+         * 
+         */
         function login(config?: LIFFLoginConfig): void
 
+        /**
+         * Logs out.
+         */
         function logout(): void
 
+        /**
+         * Opens the specified URL in the in-app browser of LINE or external browser.
+         */
         function openWindow(params: {
             url: string,
             external?: boolean
         }): void;
         
+        /**
+         * Gets the current user's access token.
+         */
         function getAccessToken(): string;
         
+        /**
+         * Gets the current user's profile.
+         */
         function getProfile(): Promise<LINE.Profile>;
         
+        /**
+         * Sends messages on behalf of the user to the chat screen where the LIFF app is opened.
+         */
         function sendMessages(messages: LINE.Message[]): Promise<void>;
         
+        /**
+         * Starts LINE's QR code reader and gets the string read by the user. To start the QR code reader, grant ScanQR permission to the LIFF app in the LINE Developers Console.
+         */
         function scanCode(): Promise<scanCodeResult>
 
+        /**
+         * Closes the LIFF app.
+         */
         function closeWindow(): void;
         
+        /**
+         * Enable the plugins.
+         *
+         * For example, if you enable the Bluetooth plugin, you can use the client API (liff.bluetooth.*) provided by the Bluetooth plugin.
+         * @param plugins - Plugin name. Specify one of the following values: `bluetooth`
+         *
+         */
         function initPlugins(plugins: LIFFPlugins[]): Promise<void>;
         
+        /**
+         * Gets the payload of the ID token that's automatically acquired by `liff.init()` The payload includes the user display name, profile image URL, and email address.
+         */
         function getDecodedIDToken(): Promise<LiffDecodedProfile>;
 
+        /**
+         * Gets the friendship status of the LINE Official Account that's linked to the LINE Login channel to which the LIFF app is added.
+         */
         function getFriendship(): Promise<Friendship>;
 
         const bluetooth: {
+            /**
+             * Check if the Bluetooth plugin can be used.
+             */
             getAvailability(): Promise<boolean>
+            /**
+             * Scans the linked device, and acquires the information.
+             * @param options
+             */
             requestDevice(options?: LIFFRequestDeviceOptions): Promise<BluetoothDevice>
         }
-    }    
+    }
 }
